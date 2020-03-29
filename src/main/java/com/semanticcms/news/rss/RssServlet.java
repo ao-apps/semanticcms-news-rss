f0d919926f32dc5cab34e1ca136737afd4330d62
AO Industries, Inc. <support@aoindustries.com>
2020-03-29 11:09:50 -0500

    Changed default number of items from 20 to 50.

--- src/main/java/com/semanticcms/news/rss/RssServlet.java
+++ src/main/java/com/semanticcms/news/rss/RssServlet.java
@@ -98,7 +98,7 @@ public class RssServlet extends HttpServlet {
 	/**
 	 * The default max items to include.
 	 */
-	private static final int DEFAULT_MAX_ITEMS = 20;
+	private static final int DEFAULT_MAX_ITEMS = 50;
 
 	/**
 	 * Gets a book parameter, null if empty.
